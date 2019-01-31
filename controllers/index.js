7cfc68ec66d5b1b6d1eb19c5bd842d180f67253e
Tan <tan@intersquad.co>
2019-01-31 17:05:42 +0700

    fix default empty array for config

--- controllers/index.js
+++ controllers/index.js
@@ -18,15 +18,15 @@ export async function saveRules(req, res) {
   if (req.body.noAct && req.body.noAct.resetPeriod) req.body.noAct.resetPeriod = parseInt(req.body.noAct.resetPeriod);
 
   if (req.body.byTag) {
-    if (req.body.byTag.whiteList) req.body.byTag.whiteList = req.body.byTag.whiteList.split(',');
-    if (req.body.byTag.blackList) req.body.byTag.blackList = req.body.byTag.blackList.split(',');
+    req.body.byTag.whiteList = req.body.byTag.whiteList ? req.body.byTag.whiteList.split(',') : [];
+    req.body.byTag.blackList = req.body.byTag.blackList ? req.body.byTag.blackList.split(',') : [];
   }
 
-  if (req.body.blockList) req.body.blockList = req.body.blockList.split(',');
+  req.body.blockList = req.body.blockList ? req.body.blockList.split(',') : [];
 
   if (req.body.vendor) {
-    if (req.body.vendor.whiteList) req.body.vendor.whiteList = req.body.vendor.whiteList.split(',');
-    if (req.body.vendor.blackList) req.body.vendor.blackList = req.body.vendor.blackList.split(',');
+    req.body.vendor.whiteList = req.body.vendor.whiteList ? req.body.vendor.whiteList.split(',') : [];
+    req.body.vendor.blackList = req.body.vendor.blackList ? req.body.vendor.blackList.split(',') : [];
   } 
 
   await services.ruleService.saveRules(req.params.vendorId, req.body);
